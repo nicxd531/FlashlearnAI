ad7a2f0049f40a18f621961e311e59a4e56a4561
Ola Olasunkanmi <finixd531@gmail.com>
2025-05-09 09:22:10 +0100

    friday morning fix

--- components/library/screens/HomeMain.tsx
+++ components/library/screens/HomeMain.tsx
@@ -4,12 +4,17 @@ import ExploreScreen from "@/components/home/ExploreScreen";
 import TopCreators from "@/components/home/reuseables/TopCreators";
 import PillToggleButton from "@/components/reuseables/PillToggleButton";
 import TopAppBar from "@/components/reuseables/TopAppBar";
-import useTabBarStore from "@/utils/store/zustand/useTabBarStore";
 import { Toasts } from "@backpackapp-io/react-native-toast";
 import { NavigationProp } from "@react-navigation/native";
 import { useNavigation } from "expo-router";
 import { FC, useEffect, useRef, useState } from "react";
-import { BackHandler, FlatList, GestureResponderEvent, RefreshControl, ScrollView } from "react-native";
+import {
+  BackHandler,
+  FlatList,
+  GestureResponderEvent,
+  RefreshControl,
+  ScrollView,
+} from "react-native";
 import { StyleSheet, View } from "react-native";
 import { Modalize } from "react-native-modalize";
 import { useQueryClient } from "react-query";
@@ -17,14 +22,9 @@ import { useQueryClient } from "react-query";
 interface Props {}
 
 const HomeMain: FC<Props> = (props) => {
-  const setScrollY = useTabBarStore((state) => state.setScrollY);
-
-  const handleScroll = (event) => {
-    setScrollY(event.nativeEvent.contentOffset.y);
-  };
-  const [refresh,setRefresh]=useState(false)
+  const [refresh, setRefresh] = useState(false);
   const navigation = useNavigation<NavigationProp<AuthStackParamList>>();
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   const modalizeRef = useRef<Modalize>(null);
   const onOpen = (event: GestureResponderEvent) => {
     modalizeRef.current?.open();
@@ -32,11 +32,15 @@ const HomeMain: FC<Props> = (props) => {
   const [activeScreen, setActiveScreen] = useState<"Explore" | "Discover">(
     "Explore"
   );
-const handleOnRefresh=()=>{
-  setRefresh(true)
-  queryClient.invalidateQueries({ queryKey: ["favorites"] });
-  setRefresh(false)
-}
+  const handleOnRefresh = () => {
+    setRefresh(true);
+    queryClient.invalidateQueries({ queryKey: ["favorites"] });
+    queryClient.invalidateQueries({ queryKey: ["topCreators"] });
+    queryClient.invalidateQueries({ queryKey: ["recentlyPlayed"] });
+    queryClient.invalidateQueries({ queryKey: ["recommended"] });
+    queryClient.invalidateQueries({ queryKey: ["feeds"] });
+    setRefresh(false);
+  };
   useEffect(() => {
     const backAction = () => {
       if (navigation.isFocused()) {
@@ -64,25 +68,32 @@ const handleOnRefresh=()=>{
         }}
       />
       <FlatList
-      ListHeaderComponent={<>
-      <TopAppBar />
-        <TopCreators />
-        <PillToggleButton
-          activeScreen={activeScreen}
-          setActiveScreen={setActiveScreen}
-        /></>}
-      data={[""]}
-      renderItem={()=>{return <> {activeScreen === "Explore" ? (
-        <ExploreScreen onOpen={onOpen} />
-      ) : (
-        <DiscoverScreen onOpen={onOpen} />
-      )}</>}}
-      showsHorizontalScrollIndicator={false} 
-      refreshControl={
-              <RefreshControl refreshing={refresh} onRefresh={handleOnRefresh} />
-            }
-            onScroll={handleScroll}
-        scrollEventThrottle={16}
+        ListHeaderComponent={
+          <>
+            <TopAppBar />
+            <TopCreators />
+            <PillToggleButton
+              activeScreen={activeScreen}
+              setActiveScreen={setActiveScreen}
+            />
+          </>
+        }
+        data={[""]}
+        renderItem={() => {
+          return (
+            <>
+              {activeScreen === "Explore" ? (
+                <ExploreScreen onOpen={onOpen} />
+              ) : (
+                <DiscoverScreen onOpen={onOpen} />
+              )}
+            </>
+          );
+        }}
+        showsHorizontalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refresh} onRefresh={handleOnRefresh} />
+        }
       />
       {/* <ScrollView showsHorizontalScrollIndicator={false}>
         <TopAppBar />
